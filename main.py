4ae57f81dcdcad1573812647dda5b65eb489d47d
hexadecimaljude <julia.volodymyrivna@icloud.com>
2025-05-16 15:02:28 +0000

    started on the main file, checks inputted directory is valid

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
 
+path = input("enter the directory of the image to convert: ")
+is_valid = os.path.isdir(path)
+if is_valid == True:
+    print("here are the images in the specified directory,", path, "\n")
+    print(os.listdir(path))
+    original = input("which image would you like to convert? ")
+else:
+    print("this is not a valid directory. please try again")
+    exit

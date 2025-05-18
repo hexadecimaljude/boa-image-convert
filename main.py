1cf22b4a57563d77f09eca688f084af04b2e7295
Jude <157845671+hexadecimaljude@users.noreply.github.com>
2025-05-18 16:24:06 +0100

    shows images only

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
 
-path = input("enter the directory of the image to convert: ")
+# you can add as many formats as you wish
+# make sure the formats are in both lower and upper case
+# (it's weird i know)
+
+formats = tuple([".png", ".PNG", ".jpg", ".JPG", ".jpeg", ".JPEG", ".heic", ".HEIC", ".gif", ".GIF"])
+
+path = input("Enter the directory of the image to convert: ")
+
+# checks if the path entered is actually an existing/valid path
+# if so, the program continues and displays any photos in the directory
+
 is_valid = os.path.isdir(path)
 if is_valid == True:
-    print("here are the images in the specified directory,", path, "\n")
-    print(os.listdir(path))
-    original = input("which image would you like to convert? ")
+    print("Here are the images in the specified directory,", path, "\n")
+    for file in os.listdir(path):
+        if (file.endswith(formats)):
+            print(file)
+    original = input("\nWhich image would you like to convert? ")
 else:
-    print("this is not a valid directory. please try again")
+    print("This is not a valid directory. Please try again")
     exit
